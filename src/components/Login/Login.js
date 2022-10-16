f8a9617f9be26647b626c212db92d2a5efbbecbe
Your Name <you@example.com>
2022-10-16 22:27:30 +0600

    login page create

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './Login.css'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div className=''>
-            <h1 className=''>Login</h1>
+        <div className='form-container'>
+            <h1 className='form-title'>Login</h1>
             <form className=''>
+                <div className="form-control">
+                    <label htmlFor='email'>Email</label>
+                    <input type="email" name="email" id="" placeholder='Your Email' required/>
+                    <br />
+                    <br />
+                    <label htmlFor='password'>Password</label>
+                    <input type="password" name="password" id="" placeholder='Your Password' required/>
+                </div>
+                <input className='btn-submit' type="submit" value="Login" />
 
             </form>
+            
+            <p> New to ema john <Link to='/signup'>Create a New Account</Link> </p>
         </div>
     );
 };
